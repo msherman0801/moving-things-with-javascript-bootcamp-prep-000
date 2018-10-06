516f7cb61a1b2e4e02479c11fd1099f808bbd37e
Matthew Sherman <msherman0801@gmail.com>
2018-10-06 17:47:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ function moveDodgerRight(e) {
   if (e.which == 37) {
     var rightNumbers = dodger.style.right.replace('px', '')
     var right = parseInt(rightNumbers, 10)
-    
-    if(right > 0) {
-    dodger.style.right = `${right - 1}px`
-    }
+
   }
 }
